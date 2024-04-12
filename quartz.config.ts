5b0c17ee010227b7e2a6fee2565f1a35ed6a890c
corvusdeinanis <25402014+corvusdeinanis@users.noreply.github.com>
2024-04-12 13:22:17 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,13 @@ import * as Plugin from "./quartz/plugins";
  */
 const config: QuartzConfig = {
 	configuration: {
-		pageTitle: "🔗 GitHub Publisher",
+		pageTitle: "Astrodex",
 		enableSPA: true,
 		enablePopovers: true,
 		analytics: {
-			provider: "plausible",
 		},
 		locale: "en-US",
-		baseUrl: "https://obsidian-publisher.netlify.app/",
+		baseUrl: "",
 		ignorePatterns: ["private", "templates", ".obsidian"],
 		defaultDateType: "created",
 		theme: {
